2ed036f96bd1f119fe2aa0abf90095c6a1985a83
teej387 <teej387@yahoo.com>
2019-07-21 23:55:28 -0500

    Created addSmurf in actions.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,10 @@ export const GET_SMURFS_START = 'GET_SMURFS_START'
 export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS'
 export const GET_SMURFS_FAILED = 'GET_SMURFS_FAILED'
 
+export const ADD_SMURF_START = 'ADD_SMURF_START'
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS'
+export const ADD_SMURF_FAILED = 'ADD_SMURF_FAILED'
+
 export function getSmurfs() {
   return (dispatch) => {
     dispatch({type: GET_SMURFS_START})
@@ -14,6 +18,20 @@ export function getSmurfs() {
       })
       .catch((err) => {
         dispatch({type: GET_SMURFS_FAILED, payload: err.response})
+      })
+  }
+}
+
+export function addSmurf(payload) {
+  return dispatch => {
+    dispatch({type: ADD_SMURF_START})
+
+    axios.post('http://localhost:3333/smurfs', payload)
+      .then(res => {
+        dispatch({type: ADD_SMURF_SUCCESS, payload: res.data})
+      })
+      .catch(err => {
+        dispatch({type: ADD_SMURF_FAILED, payload: err.response})
       })
   }
 }
